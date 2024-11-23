f5bb098ceb4cf0d55f1c2889e66acf8e45b84ba5
sajid1108 <cahsekolahbelajar@gmail.com>
2024-11-23 10:22:33 +0700

    learnjs

---
+++ variabel/index.js
@@ -0,0 +1,19 @@
+// tipe data string
+let myName = document.getElementById("name");
+myName.textContent = "Sajid Abdul Fatah";
+console.log(myName);
+
+// tipe data number
+let myAge = document.getElementById("age");
+myAge.textContent = "16";
+console.log(myAge);
+
+// tipe data boolean
+let myStatus = document.getElementById("status");
+myStatus.textContent = "pemula";
+console.log(myStatus);
+
+// tipe data school
+let mySchool = document.getElementById("school");
+mySchool.textContent = "SMK Muhammadiyah 1 SKH";
+console.log(mySchool);
