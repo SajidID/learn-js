953a8ce70f922874059167723d8f19c55ce00a66
sajid1108 <cahsekolahbelajar@gmail.com>
2025-08-14 14:50:13 +0700

    feat(variable): add type varible

--- learn-variabel/variabel.js
+++ learn-variabel/variabel.js
@@ -2,4 +2,12 @@
 
 let nama = "Budi";
 nama = "Rinz";
-console.log(nama)
+console.log(nama);
+
+// const age = 17;
+// age = 15;
+// console.log(age);
+
+var fruit = "apple";
+var fruit = "mango";
+console.log(fruit);
